1e2f41b398d7643c6ec760fc186c84e8d8255f2b
이은수 <74586127+ensu010910@users.noreply.github.com>
2024-10-04 06:10:38 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,22 +8,26 @@ def generate_text(prompt):
     inputs = tokenizer(prompt, return_tensors='pt')
     output = model.generate(inputs['input_ids'], max_length=500)
     return tokenizer.decode(output[0], skip_special_tokens=True)
+def main():
+    st.title("Chefridge Recipe Generator")
 
-st.title("Chefridge Recipe Generator")
+    input_ingredients = st.text_input("In your Refrigerator: ")
+    instruction = "Chefridge has spotted these ingredients in your fridge! What delicious recipe can you make with:"
 
-input_ingredients = st.text_input("In your Refrigerator: ")
-instruction = "Chefridge has spotted these ingredients in your fridge! What delicious recipe can you make with:"
-
-if st.button("Generate Recipe"):
-    if input_ingredients:
-        prompt = f"{instruction}\n{input_ingredients}"
+    if st.button("Generate Recipe"):
+        if input_ingredients:
+            prompt = f"{instruction}\n{input_ingredients}"
         
-        with st.spinner("Generating recipe..."):
-            try:
-                generated_text = generate_text(prompt)
-                st.write("Done! ")
-                st.write(generated_text) 
-            except Exception as e:
-                st.error(f"An error occurred: {e}") 
-    else:
-        st.warning("Please enter some ingredients!")
+            with st.spinner("Generating recipe..."):
+                try:
+                    generated_text = generate_text(prompt)
+                    st.write("Done! ")
+                    st.write(generated_text) 
+                except Exception as e:
+                    st.error(f"An error occurred: {e}") 
+        else:
+            st.warning("Please enter some ingredients!")
+
+
+if __name__ == '__main__':
+    main()
