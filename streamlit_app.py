6a0fa0962c70558decac8e99cd0ae03ca8ffbfb9
이은수 <74586127+ensu010910@users.noreply.github.com>
2024-10-04 06:15:37 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,35 @@
 import streamlit as st
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-tokenizer = AutoTokenizer.from_pretrained("baofamily/gemma-2b-finetuned-model-llama-factory")
-model = AutoModelForCausalLM.from_pretrained("baofamily/gemma-2b-finetuned-model-llama-factory")
+@st.cache_resource
+def load_model():
+    tokenizer = AutoTokenizer.from_pretrained("baofamily/gemma-2b-finetuned-model-llama-factory")
+    model = AutoModelForCausalLM.from_pretrained("baofamily/gemma-2b-finetuned-model-llama-factory")
+    return tokenizer, model
+
+tokenizer, model = load_model()
 
 def generate_text(prompt):
     inputs = tokenizer(prompt, return_tensors='pt')
     output = model.generate(inputs['input_ids'], max_length=500)
     return tokenizer.decode(output[0], skip_special_tokens=True)
-def main():
-    st.title("Chefridge Recipe Generator")
 
-    input_ingredients = st.text_input("In your Refrigerator: ")
-    instruction = "Chefridge has spotted these ingredients in your fridge! What delicious recipe can you make with:"
 
-    if st.button("Generate Recipe"):
-        if input_ingredients:
-            prompt = f"{instruction}\n{input_ingredients}"
+st.title("Chefridge Recipe Generator")
+
+input_ingredients = st.text_input("In your Refrigerator: ")
+instruction = "Chefridge has spotted these ingredients in your fridge! What delicious recipe can you make with:"
+
+if st.button("Generate Recipe"):
+    if input_ingredients:
+        prompt = f"{instruction}\n{input_ingredients}"
         
-            with st.spinner("Generating recipe..."):
-                try:
-                    generated_text = generate_text(prompt)
-                    st.write("Done! ")
-                    st.write(generated_text) 
-                except Exception as e:
-                    st.error(f"An error occurred: {e}") 
-        else:
-            st.warning("Please enter some ingredients!")
-
-
-if __name__ == '__main__':
-    main()
+        with st.spinner("Generating recipe..."):
+            try:
+                generated_text = generate_text(prompt)
+                st.write("Done! ")
+                st.write(generated_text) 
+            except Exception as e:
+                st.error(f"An error occurred: {e}") 
+    else:
+        st.warning("Please enter some ingredients!")
